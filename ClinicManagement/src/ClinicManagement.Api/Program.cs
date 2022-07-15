cc7c269b4fc601bf19bbc670f6dc49c9ce30370d
Đặng Nhật Minh <73925395+dangnhatminh09032002@users.noreply.github.com>
2022-07-15 17:06:05 +0700

    update

--- ClinicManagement/src/ClinicManagement.Api/Program.cs
+++ ClinicManagement/src/ClinicManagement.Api/Program.cs
@@ -6,14 +6,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ClinicManagement.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            using (IServiceScope scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var hostEnvironment = services.GetService<IWebHostEnvironment>();
+                var loggerFactory = services.GetService<ILoggerFactory>();
+                var logger = loggerFactory.CreateLogger<Program>();
+                try
+                {
+                    //var seedService = services.GetRequiredService<AppDbContextSeed>();
+                    //await seedService.SeedAsync(new OfficeSettings().TestDate);
+                }
+                catch (Exception ex)
+                {
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
